31a1729d240d18fd55f34be74ee8e18ca0d5c4eb
Kamiyo <79284648+Kamiyo91@users.noreply.github.com>
2022-01-11 13:00:42 +0100

    Delete Omori-Re21341 directory

--- Omori-Re21341/MapManagers/Omori2_Re21341MapManager.cs
+++
@@ -1,9 +0,0 @@
-﻿using Util_Re21341.CommonMaps;
-
-namespace Omori_Re21341.MapManagers
-{
-    public class Omori2_Re21341MapManager : BlackSilence_Re21341MapManager
-    {
-        protected override string[] CustomBGMs => new[] { "OmoriPhase1_Re21341.mp3" };
-    }
-}
--- Omori-Re21341/MapManagers/Omori4_Re21341MapManager.cs
+++
@@ -1,9 +0,0 @@
-﻿using Util_Re21341.CommonMaps;
-
-namespace Omori_Re21341.MapManagers
-{
-    public class Omori4_Re21341MapManager : BlackSilence_Re21341MapManager
-    {
-        protected override string[] CustomBGMs => new[] { "OmoriPhase2_Re21341.mp3" };
-    }
-}
