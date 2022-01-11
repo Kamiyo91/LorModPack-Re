b9f16e2ad2e82c21fad917ba73610f9a2e0c0181
Kamiyo <kamiyo@outlook.it>
2022-01-11 20:53:24 +0100

    Removing Comment

--- BLL-Re21341/Models/PreBattleUnitModel.cs
+++ BLL-Re21341/Models/PreBattleUnitModel.cs
@@ -10,11 +10,4 @@ namespace BLL_Re21341.Models
         public string SkinName { get; set; }
         public List<LorId> PassiveIds { get; set; }
     }
-    //new List<Tuple<int, List<int>, List<SephirahType>, List<string>, List<string>>>
-    //{
-    //new Tuple<int, List<int>, List<SephirahType>, List<string>, List<string>>(6,
-    //new List<int> { 10000004, 10000003 },
-    //new List<SephirahType> { SephirahType.Keter }, new List<string> { "4", "3" },
-    //new List<string> { "KamiyoNormal_Re21341", "MioNormalEye_Re21341" })
-    //};
 }
