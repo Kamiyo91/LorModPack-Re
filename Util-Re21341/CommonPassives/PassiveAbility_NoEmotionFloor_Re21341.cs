b7d8368c895afc2a1043003ee3f3ae09a559fab6
Kamiyo <kamiyo.k91@gmail.com>
2022-12-02 22:14:57 +0100

    Added Passive

---
+++ Util-Re21341/CommonPassives/PassiveAbility_NoEmotionFloor_Re21341.cs
@@ -0,0 +1,8 @@
+﻿using BigDLL4221.Passives;
+
+namespace KamiyoModPack.Util_Re21341.CommonPassives
+{
+    public class PassiveAbility_NoEmotionFloor_Re21341 : PassiveAbility_NoEmotionAndEgoFloor_DLL4221
+    {
+    }
+}
