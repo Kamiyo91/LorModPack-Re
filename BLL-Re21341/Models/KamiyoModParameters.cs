2b0062a655ed24e71383275f77b7a283404cbeaa
Kamiyo <kamiyo.k91@gmail.com>
2022-12-21 22:32:11 +0100

    Cleaning

--- BLL-Re21341/Models/KamiyoModParameters.cs
+++ BLL-Re21341/Models/KamiyoModParameters.cs
@@ -26,9 +26,9 @@ namespace KamiyoModPack.BLL_Re21341.Models
     public static class KamiyoModParameters
     {
         public const string PackageId = "LorModPackRe21341.Mod";
-        public const string SephirahBundlePackageId = "SephirahBundleSe21341.Mod";
-        public const string MaryModPackageId = "MaryIb21341.Mod";
-        public const string VortexModPackageId = "VortexTowerModSa21341.Mod";
+        //public const string SephirahBundlePackageId = "SephirahBundleSe21341.Mod";
+        //public const string MaryModPackageId = "MaryIb21341.Mod";
+        //public const string VortexModPackageId = "VortexTowerModSa21341.Mod";
         public static string Path;
         public static readonly int EgoEmotionLevel = 3;
 
@@ -72,10 +72,10 @@ namespace KamiyoModPack.BLL_Re21341.Models
             fy: 0.475f,
             originalMapStageIds: new List<LorId> { new LorId(PackageId, 4), new LorId(PackageId, 10) });
 
-        public static MapModel HayateSephirahMap = new MapModel(typeof(HayateSephirah_Re21341MapManager),
-            "Hayate_Re21341", bgy: 0.3f,
-            fy: 0.475f,
-            originalMapStageIds: new List<LorId> { new LorId(PackageId, 4), new LorId(PackageId, 10) });
+        //public static MapModel HayateSephirahMap = new MapModel(typeof(HayateSephirah_Re21341MapManager),
+        //    "Hayate_Re21341", bgy: 0.3f,
+        //    fy: 0.475f,
+        //    originalMapStageIds: new List<LorId> { new LorId(PackageId, 4), new LorId(PackageId, 10) });
 
         public static UnitModel KamiyoSoloUnit = new UnitModel(10000901, PackageId, 4);
 
@@ -92,19 +92,19 @@ namespace KamiyoModPack.BLL_Re21341.Models
             originalMapStageIds: new List<LorId> { new LorId(PackageId, 7) });
 
         //PreBattleUnits
-        public static UnitModel KamiyoPreBattleModel = new UnitModel(10000901, PackageId, 4,
-            skinName: "KamiyoNormal_Re21341", additionalPassiveIds: new List<LorId> { new LorId(PackageId, 17) });
+        //public static UnitModel KamiyoPreBattleModel = new UnitModel(10000901, PackageId, 4,
+        //    skinName: "KamiyoNormal_Re21341", additionalPassiveIds: new List<LorId> { new LorId(PackageId, 17) });
 
-        public static UnitModel MioPreBattleModel = new UnitModel(10000900, PackageId, 3,
-            skinName: "MioNormalEye_Re21341", additionalPassiveIds: new List<LorId> { new LorId(PackageId, 37) });
+        //public static UnitModel MioPreBattleModel = new UnitModel(10000900, PackageId, 3,
+        //    skinName: "MioNormalEye_Re21341", additionalPassiveIds: new List<LorId> { new LorId(PackageId, 37) });
 
-        public static List<int> EmotionCardIds = new List<int>
-            { 21341, 21342, 21343, 21344, 21345, 21346, 21347, 21348, 21349, 21350, 21351, 21352, 21353, 21354, 21355 };
+        //public static List<int> EmotionCardIds = new List<int>
+        //    { 21341, 21342, 21343, 21344, 21345, 21346, 21347, 21348, 21349, 21350, 21351, 21352, 21353, 21354, 21355 };
 
-        public static List<int> FloorEgoCardIds = new List<int>
-            { 21356, 21357, 21358, 21359, 21360 };
+        //public static List<int> FloorEgoCardIds = new List<int>
+        //    { 21356, 21357, 21358, 21359, 21360 };
 
-        public static string PoolCode = "Kurosawa_21341";
+        //public static string PoolCode = "Kurosawa_21341";
     }
 
     public class OldSamuraiUtil
