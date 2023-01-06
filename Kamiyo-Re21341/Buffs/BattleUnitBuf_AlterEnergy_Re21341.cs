5bf4be9e08c26723d4220749f3fe2bd411dc9664
Kamiyo <kamiyo.k91@gmail.com>
2023-01-07 00:21:34 +0100

    -

--- Kamiyo-Re21341/Buffs/BattleUnitBuf_AlterEnergy_Re21341.cs
+++ Kamiyo-Re21341/Buffs/BattleUnitBuf_AlterEnergy_Re21341.cs
@@ -6,6 +6,7 @@ namespace KamiyoModPack.Kamiyo_Re21341.Buffs
 {
     public class BattleUnitBuf_AlterEnergy_Re21341 : BattleUnitBuf_BaseBufChanged_DLL4221
     {
+        //private CreatureEffect Aura;
         public BattleUnitBuf_AlterEnergy_Re21341() : base(infinite: false, lastOneScene: false)
         {
         }
@@ -22,5 +23,30 @@ namespace KamiyoModPack.Kamiyo_Re21341.Buffs
                 atkDice.owner.GetActivatedCustomEmotionCard(KamiyoModParameters.PackageId, 21345, out _))
                 _owner.TakeDamage(stack, DamageType.Buf);
         }
+
+        //public override void Init(BattleUnitModel owner)
+        //{
+        //    base.Init(owner);
+        //    InitAura();
+        //}
+
+        //private void InitAura()
+        //{
+        //    Aura = SingletonBehavior<DiceEffectManager>.Instance.CreateNewFXCreatureEffect(
+        //        "5_T/FX_IllusionCard_5_T_Happiness", 1f, _owner.view, _owner.view);
+        //    foreach (var particle in Aura.gameObject.GetComponentsInChildren<ParticleSystem>())
+        //    {
+        //        if (!particle.gameObject.name.Equals("Force"))
+        //        {
+        //            particle.gameObject.SetActive(false);
+        //            continue;
+        //        }
+
+        //        var main = particle.main;
+        //        main.startColor = new Color(1, 1, 1, 0);
+        //        main.startLifetimeMultiplier = 0.5f;
+        //        main.startSizeMultiplier = 0.1f;
+        //    }
+        //}
     }
 }
